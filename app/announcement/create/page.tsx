f17481391c03f0fa0fce5436971ac9bb53e8cb8f
isahmvesel <ivesel@purdue.edu>
2025-03-16 10:59:46 -0500

    announcement creation page

---
+++ app/announcement/create/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { db } from "@/utils/firebaseConfig";
+import {
+  collection,
+  addDoc,
+  Timestamp,
+  doc,
+  updateDoc,
+  arrayUnion,
+  getDoc,
+  DocumentReference,
+} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { firebaseApp } from "@/utils/firebaseConfig";
+
+export default function CreateAnnouncement() {
+  const router = useRouter();
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  // TODO: implement functionality to retrieve group
+
+  // TODO: implement cancel button that routes user back to the group page
+
+  const createButton = async () => {
+    if (!title) {
+      alert("Announcement Title is required.");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      // create and add to database
+      const docRef = await addDoc(collection(db, "Announcements"), {
+        title,
+        body,
+        // TODO: add groupRef once groups are being stored in database
+      });
+      setTitle("");
+      setBody("");
+
+      alert("Announcement successfully created!");
+      // TODO: route to group announcement page
+    } catch (e) {
+      console.error("there was an error creating a new announcement", e);
+      alert("failed to create announcement");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center mt-4">
+      <Card className="w-full max-w-md p-6 shadow-lg bg-white rounded-xl">
+        <CardHeader>
+          <CardTitle className="text-center text-2xl font-semibold">
+            New Announcement
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <form>
+            <div className="mb-4">
+              <Label className="text-sm font-medium">Title</Label>
+              <Input
+                placeholder="Announcement Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className="mt-1"
+              ></Input>
+            </div>
+
+            <div className="mb-4">
+              <Label className="text-sm font-meidum">Body</Label>
+              <Textarea
+                placeholder="Announcement body"
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                className="mt-1"
+              ></Textarea>
+            </div>
+          </form>
+        </CardContent>
+
+        <CardFooter>
+          <Button className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all mx-3 my-0">
+            Cancel
+          </Button>
+          <Button
+            className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all mx-3 my-0"
+            onClick={createButton}
+            disabled={loading}
+          >
+            Create
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
