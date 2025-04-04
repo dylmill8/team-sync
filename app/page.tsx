f3e55c5cce981f39f065e26e313bc9434a321fbe
dylmill8 <mill3170@purdue.edu>
2025-04-04 15:45:38 -0400

    Minor change

--- app/page.tsx
+++ app/page.tsx
@@ -157,7 +157,7 @@ const HomePage = () => {
     <div className="bg-[rgb(230,230,230)] grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
         <h1 className="text-black text-center text-5xl underline text-bold">
-          Team Sync
+          Team Sync!
         </h1>
 
         <ul className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
