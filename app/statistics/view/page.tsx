20f6ac8e139b060b224f2dade2c40a15e33117e7
isahmvesel <ivesel@purdue.edu>
2025-04-23 09:12:17 -0400

    button to stats page and more stats

--- app/statistics/view/page.tsx
+++ app/statistics/view/page.tsx
@@ -4,6 +4,9 @@ import { firebaseApp, db } from "@/utils/firebaseConfig";
 import { getAuth, User } from "firebase/auth";
 import { useState, useEffect } from "react";
 import { doc, DocumentReference, getDoc } from "firebase/firestore";
+import { useRouter } from "next/navigation";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 // interface GroupData {
 //   announcements: DocumentReference[];
@@ -38,6 +41,8 @@ interface WorkoutData {
 }
 
 export default function StatisticsPage() {
+  const router = useRouter();
+
   // auth data
   const auth = getAuth(firebaseApp);
   const [user, setUser] = useState<User | null>(null);
@@ -47,7 +52,13 @@ export default function StatisticsPage() {
   const [eventsList, setEventsList] = useState<EventData[]>([]);
   const [workoutList, setWorkoutList] = useState<WorkoutData[]>([]);
 
-  // events data
+  const [loadingWorkouts, setLoadingWorkouts] = useState<boolean>(false);
+  const [loadingEvents, setLoadingEvents] = useState<boolean>(false);
+
+  // events stats
+  const [totalEvents, setTotalEvents] = useState<number>(0);
+  const [groupEvents, setGroupEvents] = useState<number>(0);
+  const [rsvpStatuses, setRSVPStatuses] = useState<number[]>([0, 0, 0]);
 
   // workouts data
 
@@ -77,61 +88,76 @@ export default function StatisticsPage() {
 
             // set user data variables
             setUsername(userData.username || null);
-            
+
             // extract workouts
+            setLoadingWorkouts(true);
             let workouts: WorkoutData[] = [];
             if (Array.isArray(userData.workouts)) {
-              const workoutPromise = userData.workouts.map(async (workoutId: string) => {
-                const workoutRef = doc(db, "Workouts", workoutId);
-                const workoutDoc = await getDoc(workoutRef);
-                return workoutDoc.data() as WorkoutData;
-              });
+              const workoutPromise = userData.workouts.map(
+                async (workoutId: string) => {
+                  const workoutRef = doc(db, "Workouts", workoutId);
+                  const workoutDoc = await getDoc(workoutRef);
+                  return workoutDoc.data() as WorkoutData;
+                }
+              );
 
               workouts = await Promise.all(workoutPromise);
-              const filteredWorkouts = workouts.filter((e) => e != null) as WorkoutData[];
+              const filteredWorkouts = workouts.filter(
+                (e) => e != null
+              ) as WorkoutData[];
               workouts = filteredWorkouts;
             }
             setWorkoutList(workouts);
+            setLoadingWorkouts(false);
 
             // extract user events
+            setLoadingEvents(true);
             let events: EventData[] = [];
             if (Array.isArray(userData.events)) {
-              const eventPromise = userData.events.map(async (eventRef: DocumentReference) => {
-                const eventDoc = await getDoc(eventRef);
-                return eventDoc.data() as EventData;
-              });
+              const eventPromise = userData.events.map(
+                async (eventRef: DocumentReference) => {
+                  const eventDoc = await getDoc(eventRef);
+                  return eventDoc.data() as EventData;
+                }
+              );
 
               const userEvents = await Promise.all(eventPromise);
-              const filteredUserEvents = userEvents.filter((e) => e != null) as EventData[];
+              const filteredUserEvents = userEvents.filter(
+                (e) => e != null
+              ) as EventData[];
               events = filteredUserEvents;
             }
 
             // extract group events
             if (Array.isArray(userData.groups)) {
-              const groupPromise = userData.groups.map(async (groupRef: DocumentReference) => {
-                const groupDoc = await getDoc(groupRef);
-                const groupData = groupDoc.data();
+              const groupPromise = userData.groups.map(
+                async (groupRef: DocumentReference) => {
+                  const groupDoc = await getDoc(groupRef);
+                  const groupData = groupDoc.data();
 
-                if (groupData?.events && Array.isArray(groupData.events)) {
-                  const events : EventData[] = [];
+                  if (groupData?.events && Array.isArray(groupData.events)) {
+                    const events: EventData[] = [];
 
-                  for (const eventRef of groupData.events) {
-                    const eventDoc = await getDoc(eventRef);
-                    events.push(eventDoc.data() as EventData);
+                    for (const eventRef of groupData.events) {
+                      const eventDoc = await getDoc(eventRef);
+                      events.push(eventDoc.data() as EventData);
+                    }
+
+                    return events;
                   }
-                  
-                  return events;
                 }
-              });
+              );
 
               const groupEventList = await Promise.all(groupPromise);
-              const filteredGroupList = groupEventList.flat().filter((e) => e != null);
-              console.log("filteredGroupList:", filteredGroupList);
-              console.log("events:", events);
+              const filteredGroupList = groupEventList
+                .flat()
+                .filter((e) => e != null);
+              setGroupEvents(filteredGroupList.length);
               events = [...events, ...filteredGroupList];
             }
             setEventsList(events);
-
+            setTotalEvents(events.length);
+            setLoadingEvents(false);
           }
         } catch (e) {
           console.log("error getting user data", e);
@@ -142,21 +168,77 @@ export default function StatisticsPage() {
     fetchUserData();
   }, [user]);
 
+  // process event data
+  useEffect(() => {
+    let yesRSVP = 0;
+    let maybeRSVP = 0;
+    let noRSVP = 0;
+
+    const readEventData = async () => {
+      const eventDataPromise = eventsList.map(async (eventData: EventData) => {
+        const eventRSVP = eventData.RSVP;
+        if (user && eventRSVP) {
+          const status = eventRSVP[user.uid];
+
+          if (status == "yes") {
+            yesRSVP += 1;
+          } else if (status == "maybe") {
+            maybeRSVP += 1;
+          } else if (status == "no") {
+            noRSVP += 1;
+          }
+        }
+      });
+      await Promise.all(eventDataPromise);
+    };
+
+    readEventData();
+
+    setRSVPStatuses([yesRSVP, maybeRSVP, noRSVP]);
+  }, [eventsList, user]);
+
   return (
-    <div>
+    <div className="m-2">
+      <p>Disclaimer this page is very much not complete I know it looks horrendous please don&apos;t judge it yet thanks</p>
+      <Button onClick={() => router.push("/profile")}>back</Button>
       {user && (
         <div>
-          <h1>Hello, {username}!</h1>
+          <Label className="font-bold text-xl">Hello, {username}!</Label>
 
-          <h2>list of workouts:</h2>
-          {workoutList.map((workoutData, i) => (
-            <p key={i}>{workoutData.name}</p>
-          ))}
+          <div className="flex w-full">
+            <div className="p-2 m-2">
+              <Label className="font-bold text-lg">Workout Stats</Label>
+              {loadingWorkouts && <p>Loading workouts...</p>}
+              {!loadingWorkouts && (
+                <div>
+                  <h2>list of workouts:</h2>
+                  {workoutList.map((workoutData, i) => (
+                    <p key={i}>{workoutData.name}</p>
+                  ))}
+                </div>
+              )}
+            </div>
 
-          <h2>list of events:</h2>
-          {eventsList.map((eventData, i) => (
-            <p key={i}>{eventData.name}</p>
-          ))}
+            <div className="p-2 m-2">
+              <Label className="font-bold text-lg">Event Stats</Label>
+              {loadingEvents && <p>Loading events...</p>}
+              {!loadingEvents && (
+                <div>
+                  <h2>total events: {totalEvents}</h2>
+                  <h2>group events: {groupEvents}</h2>
+
+                  <h2>events attended: {rsvpStatuses[0]}</h2>
+                  <h2>events missed: {rsvpStatuses[2]}</h2>
+                  <h2>events possibly attended (maybe): {rsvpStatuses[1]}</h2>
+
+                  <h2>list of events:</h2>
+                  {eventsList.map((eventData, i) => (
+                    <p key={i}>{eventData.name}</p>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       )}
 
