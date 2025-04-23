1d82b9ebbc9f942d9498efd474d51d6fcb40cc72
isahmvesel <ivesel@purdue.edu>
2025-04-23 15:58:50 -0400

    more stats stuff

--- app/statistics/view/page.tsx
+++ app/statistics/view/page.tsx
@@ -3,10 +3,17 @@
 import { firebaseApp, db } from "@/utils/firebaseConfig";
 import { getAuth, User } from "firebase/auth";
 import { useState, useEffect } from "react";
-import { doc, DocumentReference, getDoc } from "firebase/firestore";
+import { doc, DocumentReference, getDoc, Timestamp } from "firebase/firestore";
 import { useRouter } from "next/navigation";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 // interface GroupData {
 //   announcements: DocumentReference[];
@@ -22,8 +29,8 @@ import { Button } from "@/components/ui/button";
 interface EventData {
   name: string;
   allDay: boolean;
-  start: { seconds: number };
-  end: { seconds: number };
+  start: Timestamp;
+  end: Timestamp;
   description: string;
   location: string;
   docID: string;
@@ -52,13 +59,15 @@ export default function StatisticsPage() {
   const [eventsList, setEventsList] = useState<EventData[]>([]);
   const [workoutList, setWorkoutList] = useState<WorkoutData[]>([]);
 
-  const [loadingWorkouts, setLoadingWorkouts] = useState<boolean>(false);
-  const [loadingEvents, setLoadingEvents] = useState<boolean>(false);
+  const [loadingWorkouts, setLoadingWorkouts] = useState<boolean>(true);
+  const [loadingEvents, setLoadingEvents] = useState<boolean>(true);
 
   // events stats
   const [totalEvents, setTotalEvents] = useState<number>(0);
   const [groupEvents, setGroupEvents] = useState<number>(0);
   const [rsvpStatuses, setRSVPStatuses] = useState<number[]>([0, 0, 0]);
+  const [upcomingEvents, setUpcomingEvents] = useState<number>(0);
+  const [pastEvents, setPastEvents] = useState<number>(0);
 
   // workouts data
 
@@ -168,14 +177,18 @@ export default function StatisticsPage() {
     fetchUserData();
   }, [user]);
 
-  // process event data
+  // process and calculate event data
   useEffect(() => {
     let yesRSVP = 0;
     let maybeRSVP = 0;
     let noRSVP = 0;
 
+    let upcoming = 0;
+    let past = 0;
+
     const readEventData = async () => {
       const eventDataPromise = eventsList.map(async (eventData: EventData) => {
+        // rsvp
         const eventRSVP = eventData.RSVP;
         if (user && eventRSVP) {
           const status = eventRSVP[user.uid];
@@ -188,6 +201,14 @@ export default function StatisticsPage() {
             noRSVP += 1;
           }
         }
+
+        // upcoming and past events
+        if (eventData.start.toDate() > new Date()) {
+          upcoming += 1;
+        }
+        if (eventData.end.toDate() < new Date()) {
+          past += 1;
+        }
       });
       await Promise.all(eventDataPromise);
     };
@@ -195,48 +216,97 @@ export default function StatisticsPage() {
     readEventData();
 
     setRSVPStatuses([yesRSVP, maybeRSVP, noRSVP]);
+    setUpcomingEvents(upcoming);
+    setPastEvents(past);
   }, [eventsList, user]);
 
   return (
     <div className="m-2">
-      <p>Disclaimer this page is very much not complete I know it looks horrendous please don&apos;t judge it yet thanks</p>
+      <p>
+        Disclaimer this page is very much not complete I know it looks
+        horrendous please don&apos;t judge it yet thanks
+      </p>
       <Button onClick={() => router.push("/profile")}>back</Button>
       {user && (
-        <div>
-          <Label className="font-bold text-xl">Hello, {username}!</Label>
+        <div className="">
+          <Label className="font-bold text-xl p-1 m-1">
+            {username}&apos;s statistics.
+          </Label>
 
-          <div className="flex w-full">
-            <div className="p-2 m-2">
-              <Label className="font-bold text-lg">Workout Stats</Label>
-              {loadingWorkouts && <p>Loading workouts...</p>}
-              {!loadingWorkouts && (
-                <div>
-                  <h2>list of workouts:</h2>
-                  {workoutList.map((workoutData, i) => (
-                    <p key={i}>{workoutData.name}</p>
-                  ))}
-                </div>
-              )}
+          <div className="">
+            <div className="p-1 m-1">
+              <Card className="l">
+                <CardHeader>
+                  <CardTitle className="font-bold text-lg">
+                    Event Statistics
+                  </CardTitle>
+                </CardHeader>
+
+                <CardContent>
+                  {loadingEvents && <p>Loading events...</p>}
+                  {!loadingEvents && (
+                    <div className="flex">
+                      <Card className="mr-3">
+                        <CardHeader>
+                          <CardTitle>Overview</CardTitle>
+                        </CardHeader>
+
+                        <CardContent className="flex flex-col">
+                          <Label className="mb-1">
+                            Total events: {totalEvents}
+                          </Label>
+                          <Label className="mb-3">Group events: {groupEvents}</Label>
+                          <Label className="mb-1">Upcoming events: {upcomingEvents}</Label>
+                          <Label>Past events: {pastEvents}</Label>
+                        </CardContent>
+                      </Card>
+
+                      <Card className="mr-2">
+                        <CardHeader>
+                          <CardTitle>RSVP Statistics</CardTitle>
+                        </CardHeader>
+
+                        <CardContent className="flex flex-col">
+                          <Label className="mb-1">Events Attended/Attending: {rsvpStatuses[0]}</Label>
+                          <Label className="mb-1">Events Missed/Missing: {rsvpStatuses[2]}</Label>
+                          <Label>Events Possibly Attended/Attending: {rsvpStatuses[2]}</Label>
+                        </CardContent>
+                      </Card>
+                    </div>
+                  )}
+                </CardContent>
+
+                <CardFooter>
+                  <Button>Route to events or something?</Button>
+                </CardFooter>
+              </Card>
             </div>
 
-            <div className="p-2 m-2">
-              <Label className="font-bold text-lg">Event Stats</Label>
-              {loadingEvents && <p>Loading events...</p>}
-              {!loadingEvents && (
-                <div>
-                  <h2>total events: {totalEvents}</h2>
-                  <h2>group events: {groupEvents}</h2>
+            <div className="p-1 m-1">
+              <Card>
+                <CardHeader>
+                  <CardTitle className="font-bold text-lg">
+                    Workout Statistics
+                  </CardTitle>
+                </CardHeader>
 
-                  <h2>events attended: {rsvpStatuses[0]}</h2>
-                  <h2>events missed: {rsvpStatuses[2]}</h2>
-                  <h2>events possibly attended (maybe): {rsvpStatuses[1]}</h2>
+                <CardContent>
+                  {loadingWorkouts && <Label>Loading workouts...</Label>}
 
-                  <h2>list of events:</h2>
-                  {eventsList.map((eventData, i) => (
-                    <p key={i}>{eventData.name}</p>
-                  ))}
-                </div>
-              )}
+                  {!loadingWorkouts && (
+                    <div>
+                      <h2>list of workouts:</h2>
+                      {workoutList.map((workoutData, i) => (
+                        <p key={i}>{workoutData.name}</p>
+                      ))}
+                    </div>
+                  )}
+                </CardContent>
+
+                <CardFooter>
+                  <Button>Go to workouts button?</Button>
+                </CardFooter>
+              </Card>
             </div>
           </div>
         </div>
